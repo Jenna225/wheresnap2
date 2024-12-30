b2eabe65007e88040e26d0eafdfaa58357024560
Jenna <jen042252@gmail.com>
2024-12-30 23:07:13 +0800

    feat: Add register page and update login page layout

---
+++ app/function/register.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="w-[430px] h-[850px] mx-auto border-2 border-black overflow-hidden flex flex-col justify-center items-center">
+            <div className="container flex flex-col gap-8 p-4">
+                <main className=" flex flex-col gap-8 items-center w-[375px] h-[812px]">
+                    {/*  <Image*/}
+                    {/*    className="dark:invert"*/}
+                    {/*    src="/next.svg"*/}
+                    {/*    alt="Next.js logo"*/}
+                    {/*    width={180}*/}
+                    {/*    height={38}*/}
+                    {/*    priority*/}
+                    {/*/>*/}
+
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <p className="text-2xl font-bold">WhereSnap</p>
+                    <input
+                        type="text"
+                        placeholder="電子郵件/手機號碼"
+                        className="border-2 border-gray-300 rounded-xl p-2 "
+                    />
+                    <input
+                        type="text"
+                        placeholder="密碼"
+                        className="border-2 border-gray-300 rounded-xl p-2 "
+                    />
+                    <input
+                        type="text"
+                        placeholder="密碼確認"
+                        className="border-2 border-gray-300 rounded-xl p-2 "
+                    />
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <div className="flex flex-col gap-4">
+                        <a
+                            className="w-full w-[200px] rounded-full border border-gray-300 transition-colors flex items-center justify-center bg-white text-black text-sm h-12"
+                            href="/function/register.tsx"
+                        >
+                            註冊
+                        </a>
+                        <a
+                            className="w-full w-[200px] rounded-full border transition-colors flex items-center justify-center bg-black text-white text-sm h-12"
+                            href="/function/page"
+                        >
+                            登入
+                        </a>
+                    </div>
+                </main>
+            </div>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,58 +2,54 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <div>
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            <Image
-                className="dark:invert"
-                src="/next.svg"
-                alt="Next.js logo"
-                width={180}
-                height={38}
-                priority
+      <div className="w-[430px] h-[850px] mx-auto border-2 border-black overflow-hidden flex flex-col justify-center items-center">
+        <div className="container flex flex-col gap-8 p-4">
+          <main className=" flex flex-col gap-8 items-center w-[375px] h-[812px]">
+            {/*  <Image*/}
+            {/*    className="dark:invert"*/}
+            {/*    src="/next.svg"*/}
+            {/*    alt="Next.js logo"*/}
+            {/*    width={180}*/}
+            {/*    height={38}*/}
+            {/*    priority*/}
+            {/*/>*/}
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <p className="text-2xl font-bold">WhereSnap</p>
+            <input
+                type="text"
+                placeholder="電子郵件/手機號碼"
+                className="border-2 border-gray-300 rounded-xl p-2 "
             />
-            <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-              <li className="mb-2">
-                Get started by editing{" "}
-                <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-                  app/page.tsx
-                </code>
-                .
-              </li>
-              <li>Save and see your changes instantly.</li>
-            </ol>
-
-            <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <input
+                type="text"
+                placeholder="密碼"
+                className="border-2 border-gray-300 rounded-xl p-2 "
+            />
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className="flex flex-col gap-4">
               <a
-                  className=" w-20 rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                  href="/function/login"
+                  className="w-full w-[200px] rounded-full border transition-colors flex items-center justify-center bg-black text-white text-sm h-12"
+                  href="/function/main.tsx"
               >
-
-                {/*can add the little persosn image here*/}
-                {/*<Image*/}
-                {/*    className="dark:invert"*/}
-                {/*    src="/vercel.svg"*/}
-                {/*    alt="Vercel logomark"*/}
-                {/*    width={20}*/}
-                {/*    height={20}*/}
-                {/*/>*/}
                 登入
               </a>
-              <br></br>
               <a
-                  className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                  href="/function/register"
+                  className="w-full w-[200px] rounded-full border border-gray-300 transition-colors flex items-center justify-center bg-white text-black text-sm h-12"
+                  href="/function/register.tsx"
               >
                 註冊
               </a>
             </div>
           </main>
-          <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-
-
-
-          </footer>
         </div>
       </div>
   );
