957638edf4210f9b1777eed9f098025af8064ca3
Jenna <jen042252@gmail.com>
2025-01-01 19:19:19 +0800

    refactor: Adjust layout and spacing in PersonalInfoPage

--- app/page1/page.tsx
+++ app/page1/page.tsx
@@ -7,24 +7,25 @@ export default function PersonalInfoPage() {
         <div className="w-[430px] h-[850px] mx-auto border-2 border-black overflow-hidden flex flex-col">
             <div className="container flex flex-col p-4">
                 <div className="flex justify-between items-center mb-4">
-                    <Link 
-                        href="/function/main" 
+                    <Link
+                        href="/function/main"
                         className="bg-black text-white px-4 py-2 rounded-full shadow-lg"
                     >
                         返回
                     </Link>
-                    <h1 className="text-2xl font-bold">個人資訊</h1>
+
                 </div>
-                
+                <h1 className="text-2xl font-bold">個人資訊</h1>
+                <br></br>
                 <div className="flex flex-col items-center space-y-6">
                     {/* Profile Photo */}
                     <div className="relative w-32 h-32 rounded-full overflow-hidden border-2 border-gray-300">
-                        <img 
-                            src="https://picsum.photos/200/200?random=11" 
-                            alt="Profile" 
+                        <img
+                            src="https://picsum.photos/200/200?random=11"
+                            alt="Profile"
                             className="w-full h-full object-cover"
                         />
-                        <button 
+                        <button
                             className="absolute bottom-0 right-0 bg-black text-white rounded-full p-1"
                             onClick={() => alert('Change profile photo')}
                         >
@@ -52,7 +53,7 @@ export default function PersonalInfoPage() {
                     </div>
 
                     {/* Save Button */}
-                    <button 
+                    <button
                         className="w-full bg-black text-white px-4 py-2 rounded-full shadow-lg"
                         onClick={() => alert('個人資訊已保存')}
                     >
@@ -60,7 +61,7 @@ export default function PersonalInfoPage() {
                     </button>
                 </div>
             </div>
-            
+
         </div>
     );
 }
